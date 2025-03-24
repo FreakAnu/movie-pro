71d4f7b09529d03c7686cf7b8dc8b6255fef54f5
Anirudh <anirudhparab124@gmail.com>
2025-03-24 18:22:59 +0530

    added api key

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Search from "./components/Search"
 
+
+const API_BASE_URL="https://api.themoviedb.org/3"
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method:'GET',
+  headers:{
+    accept:'application/json',
+    Authorization:`Bearer ${API_KEY}`
+  }
+}
 
 function App() {
 
   const[searchTerm, setSearchTerm] = useState("")
+  const[errorMessage,setErrorMessage] = useState("")
+
+ const fetchMovies = async() =>{
+    try{
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+      const response = await fetch(endpoint,API_OPTIONS);
+      console.log(response)
+
+    }catch(error){
+      console.error(`Error fetching the movies:${error}`);
+      setErrorMessage("Error fetching the movies . please try again later");
+    }
+ }
+
+  useEffect(() =>{
+    fetchMovies()
+  },[])
 
   return (
     <main >
@@ -18,6 +46,11 @@ function App() {
       </header>
       <Search searchtext={searchTerm} setSearchText={setSearchTerm}/>
       </div>
+      <section className="all-movies">
+        <h2>All movies</h2>
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+      </section>
+     
     </main>
   )
 }
